013a48b2a9919aedf0cc5930a3f11ad9b3de6e41
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 17:33:57 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,12 +43,16 @@ def naming_system
             :label=> "Family",
             :sub_category=> {
               :label=> "Genus",
-              :sub_category=> {
-    },
-    :
-    :label=> "Species",
-      :sub_category=> nil
+              :sub_category=> { 
+                :label=> "Species",
+                :sub_category=> nil
+              }
+            }
+          }
+        }
+      }
     }
+  }
   
   # Remember:
   #  Kingdom
