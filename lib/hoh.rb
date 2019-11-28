d1cd7f4ade4c3522f6112ba155a5eaf72f1aa613
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 17:32:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,13 +36,14 @@ def naming_system
     :sub_category=> {
       :label=> "Phylum",
       :sub_category=> {
-        :label=> "Class"
+        :label=> "Class",
         :sub_category=> {
-          :label=> "Order"},
-          :sub_category=> {},
-    :label=> "Family",
-      :sub_category=> {
-      :label=> "Genus"
+          :label=> "Order"
+          :sub_category=> {
+            :label=> "Family",
+            :sub_category=> {
+              :label=> "Genus",
+              :sub_category=> {
     },
     :
     :label=> "Species",
