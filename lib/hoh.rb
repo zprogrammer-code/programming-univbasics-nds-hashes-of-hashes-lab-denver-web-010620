4ca31bc755e995b067d0005648abd5e3d4c50ec7
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 17:21:41 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,11 +41,11 @@ def naming_system
     :label=> "Order"
       },
     :label=> "Family",
-    :sub_category=> {
+      :sub_category=> {
       :label=> "Genus"
     },
     :label=> "Species",
-    :sub_category=> nil
+      :sub_category=> nil
     }
   
   # Remember:
