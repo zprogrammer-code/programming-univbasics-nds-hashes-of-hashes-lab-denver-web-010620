288f944ce513e5b62fd7e0e9ae51321edf2c0bac
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 17:37:38 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,29 @@
 # }
 
 def naming_system
+  science = {
+    :label=> "Kingdom",
+    :sub_category=> {
+      :label=> "Phylum",
+      :sub_category=> {
+        :label=> "Class",
+        :sub_category=> {
+          :label=> "Order",
+          :sub_category=> {
+            :label=> "Family",
+            :sub_category=> {
+              :label=> "Genus",
+              :sub_category=> { 
+                :label=> "Species",
+                :sub_category=> nil
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  
   # Remember:
   #  Kingdom
   #  Phylum
